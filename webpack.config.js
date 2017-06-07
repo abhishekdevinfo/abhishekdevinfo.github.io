c2f9957f3e4e1127c348a881c8988f446ef146ab
Abhishek Baliyan <abhi.info.dev@gmail.com>
2017-06-07 13:46:22 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const webpack = require('webpack');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: {
+    vendor: ['jquery'],
+    main: './src/main.js'
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'jshint-loader'
+          }
+        ]
+      },
+      {
+        test: /\.sass$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader', options: { sourceMap: true } }, 
+            { loader: 'postcss-loader', options: { sourceMap: true } },
+            { loader: 'sass-loader', options: { sourceMap: true } }
+          ]        
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('main.css'),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity
+    }),
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      files: ['./*.html','./dist/*.css'],
+      server: { baseDir: ['./']}
+    })
+  ],
+  watch: true,
+  devtool: 'source-map'
+}
